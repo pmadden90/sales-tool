24b83d2d95d95c204fe28c77359c8ec961098798
Paul Madden <p.madden90@gmail.com>
2019-03-20 20:38:58 +0000

    Initial commit

--- js/script.js
+++ js/script.js
@@ -33,5 +33,26 @@ $(document).ready(function() {
   } else {
     text.style.display = "none";
   }
+  
+  
+  
 }
 });
+
+//When user scrolls page, execute myFunction
+  //window.onscroll = function() {myFunction()};
+  
+  //Get the header
+  //var header = document.getElementById("myHeader");
+  
+  //Get the offset position of the navbar
+  //var sticky = header.offsetTop;
+  
+  //Add sticky class to header when you reach its scroll position. Remove "sticky"
+  //when you leave scroll position
+  //function myFunction() {
+  	//if (window.pageYOffset > sticky) {
+  	//	header.classList.add("sticky");
+  	//} else {
+  	//	header.classList.remove("sticky");
+  	//}
